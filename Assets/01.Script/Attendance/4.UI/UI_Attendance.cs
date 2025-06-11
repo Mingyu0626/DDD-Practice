4f3e0901fbcef4b07d1fca053770830612f06bc0
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-06-11 16:07:44 +0900

    Feat | Slot 구현중

--- Assets/01.Script/Attendance/4.UI/UI_Attendance.cs
+++ Assets/01.Script/Attendance/4.UI/UI_Attendance.cs
@@ -1,26 +1,51 @@
 using System.Collections.Generic;
-using System.Linq;
+using TMPro;
+using Unity.FPS.Game;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UI_Attendance : MonoBehaviour
 {
-    [SerializeField]
-    private List<UI_AttendanceSlot> _slots;
-    [SerializeField]
-    private GameObject _attendacneSlotUIPrefab;
+    [Header("# Panel Info Texts")]
+    [SerializeField] private TextMeshProUGUI StreakInfoText;
+
+    [Header("# Slots")]
+    [SerializeField] private List<UI_AttendanceSlot> _slots;
+    [SerializeField] private List<UI_AttendanceSlot> _streakSlots;
+    //[SerializeField]
+    //private GameObject _attendacneSlotUIPrefab;
 
     private void Start()
     {
-        
+        Init();
+
+        EventManager.AddListener<AttendanceRefreshEvent>(RefreshAttendanceSlots);
+        EventManager.AddListener<StreakAttendanceRefreshEvent>(RefreshStreakAttendanceSlots);
     }
 
     private void Init()
     {
-        foreach (var slot in _slots)
+        for(int i=0; i<_slots.Count; ++i)
         {
-            slot.
+            _slots[i].InitData(AttendanceManager.Instance.GetRewardData(i));
+        }
+        for (int i = 0; i < _streakSlots.Count; ++i)
+        {
+            _slots[i].InitData(AttendanceManager.Instance.GetRewardData(i)); // 나중에 streak용으로 수정
         }
     }
 
+    public void OnClickClaimRewardButton()
+    {
+        AttendanceManager.Instance.TryGetReward();
+    }
+
+    private void RefreshAttendanceSlots(AttendanceRefreshEvent evt)
+    {
+        
+    }
+
+    private void RefreshStreakAttendanceSlots(StreakAttendanceRefreshEvent evt)
+    {
+
+    }
 }
--- Assets/01.Script/Attendance/4.UI/UI_AttendanceSlot.cs
+++ Assets/01.Script/Attendance/4.UI/UI_AttendanceSlot.cs
@@ -1,17 +1,22 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_AttendanceSlot : MonoBehaviour
 {
-    
-
+    [SerializeField] private Image _icon;
+    [SerializeField] TextMeshProUGUI _dayText;
+    [SerializeField] TextMeshProUGUI _amountText;
 
     public void InitData(AttendanceRewardSO attendanceReward)
     {
-
+        _dayText.text = attendanceReward.RewardDay.ToString();
+        _amountText.text = attendanceReward.RewardAmount.ToString();
     }
 
     public void InitData(StreakAttendanceRewardSO streakAttendanceReward)
     {
-
+        _dayText.text = streakAttendanceReward.RewardDay.ToString();
+        _amountText.text = streakAttendanceReward.RewardAmount.ToString();
     }
 }
